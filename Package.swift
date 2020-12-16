698b80cde26214ff20fe2b2a690e552a94ce51d7
Tim Condon <0xTim@users.noreply.github.com>
2020-12-16 16:02:10 +0000

    Remove .git from manifest

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .library(name: "SQLKitBenchmark", targets: ["SQLKitBenchmark"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.0.0"),
     ],
     targets: [
         .target(name: "SQLKit", dependencies: [
